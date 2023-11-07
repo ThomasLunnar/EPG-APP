f96188d46495983353aa557f2c066763d329fdd5
Thomas Kruchelski <thomas@lunnar.team>
2023-11-07 16:59:43 -0300

    working login but wrong direct

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
+
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
@@ -7,8 +11,37 @@ import LogoSvg from '@assets/logo-coroa.svg';
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export function SignIn() {
+
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: IOS_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes:['profile','email']
+  });
+
+  function handleGoogleSignIn(){
+    setIsAuthenticating (true);
+
+    googleSignIn().then((response) => {
+      if(response.type !== "success"){
+        setIsAuthenticating(false);
+      }
+    })
+  }
+
+  useEffect(() => {
+    if(response?.type === 'success'){
+      if(response.authentication?.idToken){
+        console.log('Token =>', response.authentication.idToken)
+      }
+    }
+  },[response]);
 
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
@@ -52,7 +85,11 @@ export function SignIn() {
             secureTextEntry
           /> */}
 
-          <Button title="Entrar com Conta Google" />
+          <Button 
+            title="Entrar com Conta Google" 
+            onPress={handleGoogleSignIn}
+            isLoading={isAuthenticating}
+          />
         </Center>
 
         <Center mt={24}>
