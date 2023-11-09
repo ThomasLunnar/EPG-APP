ed11b7c108c5595192cec2e1c4769fdd34b30cb0
Thomas Kruchelski <thomas@lunnar.team>
2023-11-09 11:59:14 -0300

    update Googleauth function

--- src/components/GoogleAuth.tsx
+++ src/components/GoogleAuth.tsx
@@ -10,31 +10,34 @@ export default function () {
     scopes: ['https://www.googleapis.com/auth/drive.readonly'],
     webClientId: WEB_CLIENT_ID
   });
+  
+   async function signIn() {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      //   setState({ userInfo });
+      // informações do usuário simplificada, não são as mesmas que obtemos com jwt do token
+      console.log(JSON.stringify(userInfo, null, 2))
+
+
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  }
 
   return (
     <GoogleSigninButton
       size={GoogleSigninButton.Size.Wide}
       color={GoogleSigninButton.Color.Dark}
-      onPress={async () => {
-        try {
-          await GoogleSignin.hasPlayServices();
-          const userInfo = await GoogleSignin.signIn();
-          //   setState({ userInfo });
-          // informações do usuário simplificada, não são as mesmas que obtemos com jwt do token
-          console.log(JSON.stringify(userInfo, null, 2))
-        } catch (error: any) {
-          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-            // user cancelled the login flow
-          } else if (error.code === statusCodes.IN_PROGRESS) {
-            // operation (e.g. sign in) is in progress already
-          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-            // play services not available or outdated
-          } else {
-            // some other error happened
-          }
-        }
-      }}
-
+      onPress={signIn}
     />
   )
 }
